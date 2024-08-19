fd0672c1fb4252dd02f663a159a8e0500a95bcb0
KrianZhang <162588103+KrianZhang@users.noreply.github.com>
2024-08-19 12:31:57 +0100

    Update ListPDFFields.java

--- Dissertation/src/main/java/ListPDFFields.java
+++ Dissertation/src/main/java/ListPDFFields.java
@@ -9,17 +9,17 @@ import java.util.List;
 public class ListPDFFields {
     public static void main(String[] args) {
         try {
-            // 加载PDF文件
+            // Loading PDF files
             PDDocument document = PDDocument.load(new File("C:/Users/13016/Desktop/Dissertation/ir-template.pdf"));
-            // 获取PDF中的表单
+            // Get Forms in PDF
             PDAcroForm form = document.getDocumentCatalog().getAcroForm();
 
             if (form != null) {
-                // 获取并遍历所有字段
+                // Get and iterate over all fields
                 List<PDField> fields = form.getFields();
                 System.out.println("Field Names Found in the Form:");
                 for (PDField field : fields) {
-                    // 打印字段名和其他可能有用的信息
+                    // Printing field names and other information that may be useful
                     System.out.println("Field name: " + field.getFullyQualifiedName());
                     System.out.println("Field value: " + field.getValueAsString());
                     System.out.println("Field type: " + field.getFieldType());
@@ -28,7 +28,6 @@ public class ListPDFFields {
                 System.out.println("No AcroForm found in PDF document.");
             }
 
-            // 关闭文档
             document.close();
         } catch (IOException e) {
             e.printStackTrace();
